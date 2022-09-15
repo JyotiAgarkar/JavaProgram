6593d7463eb7502b7fd6e09eff42d465205af937
PCS <PCS@DESKTOP-2PJ77R5>
2022-09-15 21:11:10 +0530

    hello

---
+++ JavaProgram/src/JavaPracticeCode/Sampel1.java
@@ -0,0 +1,16 @@
+package JavaPracticeCode;
+
+public class Sampel1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="jyoti";
+		String s1=s.substring(0,3);
+		System.out.println(s1);
+		
+		char c=s.charAt(2);
+		System.out.println(c);
+	}
+
+}
